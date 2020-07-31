0385eb4acc44f8eb1114e934489c0b0ef7731021
lehar4147 <51169402+lehar4147@users.noreply.github.com>
2020-07-31 18:07:25 -0400

    Update back_end.py

--- back_end.py
+++ back_end.py
@@ -1,49 +1,74 @@
 import json
 
 class Question:
-    #txt is a list of the questions
-    #ans is an Answer object (contains list of answers)
-    def __init__(self, txt, ans):
-        self.txt = txt
-        self.ans = ans
-    def addAnswer(self,ansText):
-        self.ans.txt.append(ansText)
-class Answer:
-    #txt is a list of answers
-    #type is the type of answers
-    def __init__(self,type):
-        self.txt = []
+    #class variables
+    #prompt, type, answer
+    def __init__(self, prompt, type, ans):
+        self.prompt = prompt
         self.type = type
+        self.ans = ans
 
-#JSON
-#overarching question is the key
-#array contains the answer type and if needed subquestions to display on same page
-with open('questions.json') as jsonFile:
-    dictFile = json.loads(jsonFile.read())
+class Condition:
+    #class variables
+    #exists, question, answer
+    def __init__(self, exists, question, ans):
+        self.exists = exists
+        self.question = question
+        self.ans = ans
 
-listOfQs = []
+class Screen:
+    #class variables
+    #title, condition{}, questions[]
+    def __init__(self, title, condition, questions):
+        self.title = title
+        self.condition = condition
+        self.questions = questions
 
-#parsing dictionary intro question objects which contain answer objects
-for x in dictFile:
-    #x is the key
-    #dictFile[x] is the value
-    #Greeting
-    if (len(dictFile[x]) == 0):
-        ans = Answer("")
-        listOfText = []
-        listOfText.append(x)
-        q = Question(listOfText, ans)
-    #Single Question or Multiple Questions
+class Backend:
+    def __init__(self, screens):
+        self.screens = screens
+    #def getInitialScreen()
+        #returns Screen object
+    #def getNextScreen(self, screen):
+        #returns Screen object
+        #returns null if there are no more screens
+    #def getPrevScreen(self, screen):
+        #returns Screen object
+
+#Convert JSON into a python dictionary
+with open('sample2.json') as jsonFile:
+    data = json.load(jsonFile)
+
+parsedScreens = []
+
+#Parsing
+inputScreens = data["screens"]
+for screen in inputScreens:
+    if "condition" in screen:
+        c = Condition(True, screen["condition"]["question"], screen["condition"]["answer"])
     else:
-        ans = Answer(dictFile[x][0])
-        listOfText = []
-        listOfText.append(x)
-        for i in range(1,len(dictFile[x])):
-            listOfText.append(dictFile[x][i])
-        q = Question(listOfText, ans)
-    listOfQs.append(q)
+        c = Condition(False, "", "")
+    parsedQuestions = []
+    for question in screen["questions"]:
+        q = Question(question["prompt"], question["type"], "")
+        parsedQuestions.append(q)
+    s = Screen(screen["title"], c, parsedQuestions)
+    parsedScreens.append(s)
 
-for q in listOfQs:
-    print("Questions: " + str(q.txt))
-    print("Answer Type: " + q.ans.type)
-    print("Answers: " + str(q.ans.txt) + "\n")
+#Backend object contains all values needed
+be = Backend(parsedScreens)
+
+#Testing Output
+#Can delete later, just to show how to access values
+for screen in be.screens:
+    print("Screen Title:", screen.title)
+    if screen.condition.exists:
+        print("Condition Question:", screen.condition.question)
+        print("Condition Answer:", screen.condition.ans)
+    for question in screen.questions:
+        print("Question Prompt:", question.prompt)
+        print("Answer Type:", question.type)
+        #print("Answer:", question.ans)
+    print()
+
+print("finished")
