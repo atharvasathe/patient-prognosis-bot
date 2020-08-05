f923b6e105cbf4d602647f2e437c1f0a0afcc218
lehar4147 <51169402+lehar4147@users.noreply.github.com>
2020-08-05 19:28:55 -0400

    Update back_end.py

--- back_end.py
+++ back_end.py
@@ -2,26 +2,26 @@ import json
 
 class Question:
     #class variables
-    #prompt, type, answer
-    def __init__(self, prompt, type, ans):
+    #strin prompt, string type, string answer
+    def __init__(self, prompt, type, ans, display):
         self.prompt = prompt
         self.type = type
         self.ans = ans
+        self.display = display
 
 class Condition:
     #class variables
-    #exists, question, answer
-    def __init__(self, exists, question, ans):
-        self.exists = exists
+    #string question, string answer
+    def __init__(self, question, ans):
         self.question = question
         self.ans = ans
 
 class Screen:
     #class variables
-    #title, condition{}, questions[]
-    def __init__(self, title, condition, questions):
+    #string title, conditions[], questions[]
+    def __init__(self, title, conditions, questions):
         self.title = title
-        self.condition = condition
+        self.conditions = conditions
         self.questions = questions
 
 class Backend:
@@ -30,38 +30,84 @@ class Backend:
         self.screens = []
         self.process()
     def getInitialScreen(self):
+        self.index = 0
         return self.screens[0]
     def getNextScreen(self):
-        if ( self.index + 1 < len(self.screens) ):
-            self.index+=1
+        self.index+=1
+        self.checkConds(True)
+        if ( self.index < len(self.screens) ):
             return self.screens[self.index]
         else:
             return None
     def getPrevScreen(self):
+        self.index-=1
+        self.checkConds(False)
         if ( self.index >= 0 ):
-            self.index-=1
             return self.screens[self.index]
         else:
             return None
+    def checkConds(self, forward):
+        #Check if index is in range
+        if ( self.index < len(self.screens) and self.index > 0 ):
+            conditionList = self.screens[self.index].conditions
+            prevQuestionList = self.screens[self.index - 1].questions
+            currQuestionList = self.screens[self.index].questions
+            #Make sure conditions exist
+            if len(conditionList) > 0:
+                #Iterate to next screen (one condition)
+                if len(conditionList) == 1:
+                    if conditionList[0].ans != prevQuestionList[0].ans:
+                        if (forward):
+                            self.getNextScreen()
+                        else:
+                            self.getPrevScreen()
+                #Hide specific questions from screen
+                elif len(conditionList) == len(prevQuestionList) == len(currQuestionList):
+                    size = len(conditionList)
+                    numHidden = 0
+                    for i in range(size):
+                        if conditionList[i].ans != prevQuestionList[i].ans:
+                            currQuestionList[i].display = False
+                            numHidden+=1
+                    #All questions were hidden
+                    if ( numHidden == size ):
+                        if (forward):
+                            self.getNextScreen()
+                        else:
+                            self.getPrevScreen()
+                    if ( self.index < len(self.screens) ):
+                        self.screens[self.index].questions = currQuestionList
+                #Iterate to next screen (multiple conditions)
+                elif len(conditionList) == len(prevQuestionList):
+                    skip = False
+                    size = len(conditionList)
+                    for i in range(size):
+                        if conditionList[i].ans != "" and conditionList[i].ans != prevQuestionList[i].ans:
+                            skip = True
+                            break
+                    if (skip):
+                        if (forward):
+                            self.getNextScreen()
+                        else:
+                            self.getPrevScreen()
     def process(self):
         #Convert JSON into a python dictionary
         with open('sample2.json') as jsonFile:
             data = json.load(jsonFile)
 
-        parsedScreens = []
-
         #Parsing
+        parsedScreens = []
         inputScreens = data["screens"]
         for screen in inputScreens:
-            if "condition" in screen:
-                c = Condition(True, screen["condition"]["question"], screen["condition"]["answer"])
-            else:
-                c = Condition(False, "", "")
+            parsedConditions = []
+            for condition in screen["conditions"]:
+                c = Condition(condition["question"], condition["answer"])
+                parsedConditions.append(c)
             parsedQuestions = []
             for question in screen["questions"]:
-                q = Question(question["prompt"], question["type"], "")
+                q = Question(question["prompt"], question["type"], question["answer"], True)
                 parsedQuestions.append(q)
-            s = Screen(screen["title"], c, parsedQuestions)
+            s = Screen(screen["title"], parsedConditions, parsedQuestions)
             parsedScreens.append(s)
 
         self.screens = parsedScreens
@@ -71,54 +117,54 @@ class Backend:
 
 #Dummy Front End
 be = Backend()
-be.process()
 
-for screen in be.screens:
-    print("Screen Title:", screen.title)
-    if screen.condition.exists:
-        print("Condition Question:", screen.condition.question)
-        print("Condition Answer:", screen.condition.ans)
-    for question in screen.questions:
+#Forward Loop WITHOUT Conditions
+#print("FORWARD")
+#for scr in be.screens:
+    #print("Screen Title: ", scr.title)
+    #for condition in scr.conditions:
+        #print("Condition Question:", condition.question)
+        #print("Condition Answer:", condition.ans)
+    #for question in scr.questions:
+        #print("Question Prompt:", question.prompt)
+        #print("Answer Type:", question.type)
+        #print("Answer:", question.ans)
+        #print("Display:", question.display)
+    #print()
+
+#Forward Loop WITH Conditions
+print("FORWARD")
+scr = be.getInitialScreen()
+numScreens = len(be.screens)
+while(be.index < numScreens) :
+    print("Screen", be.index, ":")
+    print("Screen Title: ", scr.title)
+    for condition in scr.conditions:
+        print("Condition Question:", condition.question)
+        print("Condition Answer:", condition.ans)
+    for question in scr.questions:
         print("Question Prompt:", question.prompt)
         print("Answer Type:", question.type)
         print("Answer:", question.ans)
+        print("Display:", question.display)
     print()
+    scr = be.getNextScreen()
 
-#Practice Screens
-scr1 = be.getInitialScreen()
-print("Screen 1:")
-print("Screen Title:", scr1.title)
-if scr1.condition.exists:
-    print("Condition Question:", scr1.condition.question)
-    print("Condition Answer:", scr1.condition.ans)
-for question in scr1.questions:
-    print("Question Prompt:", question.prompt)
-    print("Answer Type:", question.type)
-    print("Answer:", question.ans)
-print()
-
-scr2 = be.getNextScreen()
-print("Screen 2:")
-print("Screen Title:", scr2.title)
-if scr2.condition.exists:
-    print("Condition Question:", scr2.condition.question)
-    print("Condition Answer:", scr2.condition.ans)
-for question in scr2.questions:
-    print("Question Prompt:", question.prompt)
-    print("Answer Type:", question.type)
-    print("Answer:", question.ans)
-print()
-
-scr3 = be.getPrevScreen()
-print("Screen 1 again:")
-print("Screen Title:", scr3.title)
-if scr3.condition.exists:
-    print("Condition Question:", scr3.condition.question)
-    print("Condition Answer:", scr3.condition.ans)
-for question in scr3.questions:
-    print("Question Prompt:", question.prompt)
-    print("Answer Type:", question.type)
-    print("Answer:", question.ans)
-print()
+#Backwards Loop
+print("BACKWARDS")
+scr = be.getPrevScreen()
+while(be.index >= 0):
+    print("Screen", be.index, ":")
+    print("Screen Title: ", scr.title)
+    for condition in scr.conditions:
+        print("Condition Question:", condition.question)
+        print("Condition Answer:", condition.ans)
+    for question in scr.questions:
+        print("Question Prompt:", question.prompt)
+        print("Answer Type:", question.type)
+        print("Answer:", question.ans)
+        print("Display:", question.display)
+    print()
+    scr = be.getPrevScreen()
 
 print("finished")
