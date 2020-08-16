497995007488a03b338d8ceffdefa41acb510e85
atharvasathe <atharvasathe.college@gmail.com>
2020-08-16 15:59:18 -0700

    Update front_end.py

--- front_end.py
+++ front_end.py
@@ -40,15 +40,22 @@ class StartPage(tk.Frame):
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
+        #Array Widgets holds all of the tkinter Labels and Entry box widgets 
         self.widgets=[]
-        #self.page_number=1
-        self.backend = Backend()
-        #self.screen = Screen("")
-        self.buildInitialPage()
-        #self.buildPage(self.screen)
 
+        #initializes backend
+        self.backend = Backend()
+
+        #builds the first screen
+        self.buildInitialPage()
+
+        #Dictionary that holds all of the answer widgets
+        self.answers_dictionary={}
+
+
+    #builds initial screen
+    #is only called at the beginning
     def buildInitialPage(self):
-        #self.screen = self.screen.getInitialScreen()
         screen = self.backend.getInitialScreen()
         self.buildPage(screen)
 
@@ -76,32 +83,43 @@ class StartPage(tk.Frame):
             button1.pack()
             self.widgets.append(button1) """
         current_screen = screen
+
+        #packs the screen title in larger font
         label = tk.Label(self, text=current_screen.title, font=TITLE_FONT)
         label.pack(pady=10, padx=10)
+        #adds the screen title to array widgets
         self.widgets.append(label)
 
+        #loops through all Questions in Screen and displays them 
         for question in screen.questions:
             self.displayQuestion(question)
 
+        #next button calls for the next screen
         button_next = tk.Button(self, text="Next", 
                                 command=lambda: self.nextPage(current_screen, 1))
         button_next.pack()
+        #adds next button to array widgets
         self.widgets.append(button_next)
-
+        #prev button calls for the prev screen
         button_prev = tk.Button(self, text="Back", 
                                 command=lambda: self.nextPage(current_screen, 0))
         button_prev.pack()
+        #adds prev button to array widgets
         self.widgets.append(button_prev)
 
-        
+       
     def destroyPage(self):
+        #loops through array widgets and deletes them
         for x in self.widgets:
             x.pack_forget()
+        #clears array widgets
         self.widgets=[]
 
     def nextPage(self, screen, direction):
+        self.saveAnswers(screen)
         self.destroyPage()
-        #self.page_number=self.page_number+1
+
+        #calls next or prev depending on the direction parameter
         if direction == 1:
             self.buildPage(self.backend.getNextScreen())
         else:
@@ -109,24 +127,46 @@ class StartPage(tk.Frame):
        
 
     def displayQuestion(self, question):
+        #displays question prompt
         question_prompt = tk.Label(self, text=question.prompt, font= QUESTION_FONT)
         question_prompt.pack()#side = LEFT)
+        #adds question prompt to array widgets
         self.widgets.append(question_prompt)
 
+        #creates the proper entry widget depending on the answer type
         if question.type == "string":
             string_entry = tk.Entry(self, bd=2)
             string_entry.pack()#side = RIGHT)
             self.widgets.append(string_entry)
+            self.answers_dictionary[question.prompt] = string_entry
         elif question.type == "yn":
-            var = tk.IntVar()
-            radio_entry_yes = tk.Radiobutton(self, text="Yes", variable=var, value=1)
+            var = tk.StringVar()
+            self.answers_dictionary[question.prompt] = var
+            radio_entry_yes = tk.Radiobutton(self, text="Yes", variable=var, value="yes")
             radio_entry_yes.pack()#side=RIGHT)
             self.widgets.append(radio_entry_yes)
 
-            radio_entry_no = tk.Radiobutton(self, text="No", variable=var, value=2)
+            radio_entry_no = tk.Radiobutton(self, text="No", variable=var, value="no")
             radio_entry_no.pack()#side=RIGHT)
             self.widgets.append(radio_entry_no)
+        elif question.type == "check":
+            var = tk.IntVar()
+            self.answers_dictionary[question.prompt] = var
+            check_button = tk.Checkbutton(self, variable = var, onvalue = 1, offvalue = 0)
+            check_button.pack()
+            self.widgets.append(check_button)
             
+
+    def saveAnswers(self, screen):
+        #loops through Questions and saves the answers depending on dictionary answers
+        for question in screen.questions:
+            if question.type == "yn":
+                if self.answers_dictionary[question.prompt] == "yes":
+                    question.ans = "yes"
+                else: 
+                    question.ans = "no"
+            else:
+                question.ans = self.answers_dictionary[question.prompt].get()                    
 
 
 
